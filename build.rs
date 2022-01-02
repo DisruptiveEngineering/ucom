0a35d4b9c5f2e5d9c022e7b887fdfe46c793b9ba
sander <sander.wittwer@dengineering.no>
2022-01-02 17:02:45 +0100

    cargo fmt

--- build.rs
+++ build.rs
@@ -1,12 +1,11 @@
 use clap::{ArgEnum, IntoApp};
 use clap_complete::{generate_to, Shell};
 
-#[path= "src/opts.rs"]
+#[path = "src/opts.rs"]
 #[allow(dead_code)]
 mod opts;
 
 fn main() {
-
     // Generate shell completions
     let mut app = opts::Opts::into_app();
     let bin_name: String = app.get_name().to_string();
