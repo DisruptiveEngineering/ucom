4539189b560afd77c0a99277cc47c4de63b3cdd5
sander <sander.wittwer@dengineering.no>
2022-01-02 21:03:07 +0100

    build.rs: use std PathBuf

--- build.rs
+++ build.rs
@@ -10,10 +10,11 @@ fn main() {
     let mut app = opts::Opts::into_app();
     let bin_name: String = app.get_name().to_string();
 
-    let comp_path = format!("{}/completions", std::env::var("OUT_DIR").unwrap());
+    let comp_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap_or_default())
+        .join(std::path::PathBuf::from("completions"));
 
     for shell in Shell::value_variants() {
-        let out_dir = format!("{}/{}", comp_path, shell.to_string());
+        let out_dir = comp_path.join(shell.to_string());
         std::fs::create_dir_all(&out_dir).unwrap();
 
         generate_to(*shell, &mut app, &bin_name, out_dir).unwrap();
